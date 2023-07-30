9e1e7cba5eb1bc7c8705c27055ba8942a3944d5d
Xavpunkxd <jon20xavi@gmail.com>
2023-07-30 16:36:31 -0600

    Version 1

---
+++ src/main/java/com/mycompany/conversor/Conversor.java
@@ -0,0 +1,13 @@
+package com.mycompany.conversor;
+import Screen.WindowConverter;
+ 
+public class Conversor {
+
+    public static void main(String[] args) {
+        WindowConverter screeninit = new WindowConverter();
+        screeninit.setVisible(true);
+        screeninit.setLocationRelativeTo(null);
+        screeninit.setResizable(false);
+       
+    }
+}
